62f2a25fcce0716d04551b3cfebb76c6683845ba
Manarisia <okaforoluoma36@gmail.com>
2023-06-29 11:51:08 -0700

    manarisiareact

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,10 @@ export default function Home(){
     return<>
    <h1 className='text-center m-5'>REACT PROJECT</h1>
   <div className="projects d-flex flex-column align-items-center">
-    <a href="/todo-app" className='project'>Todo App</a>
-    <a href="/expense-tracker" className='project'>Expense Tracker</a>
-    <a href="/fake-store" className='project'>Fake Store (API)</a>
-    <a href="/movie-website" className='project'>Movie Website (API)</a>
+    <Link to="/todo-app" className='project'>Todo App</Link>
+    <Link to="/expense-tracker" className='project'>Expense Tracker</Link>
+    <Link to="/fake-store" className='project'>Fake Store (API)</Link>
+    <Link to="/movie-website" className='project'>Movie Website (API)</Link>
   </div>
 
     </>
